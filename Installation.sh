de949d1b00c50d7a0bda8075e33e63736d2c3c6b
vahid <v.babaee@Gmail.com>
2020-04-05 20:38:54 +0430

    کد نصب پیش نیاز های مربوط به کد ربات خودکار

---
+++ Installation.sh
@@ -0,0 +1,16 @@
+python3 -m pip install setuptools
+python -c 'print("---------------")'
+python3 -m pip install requests
+python -c 'print("---------------")'
+python3 -m pip install BeautifulSoup4
+python -c 'print("---------------")'
+python3 -m pip install jsons
+python -c 'print("---------------")'
+python3 -m pip install tinydb
+python -c 'print("---------------")'
+python3 -m pip install selenium
+python -c 'print("---------------")'
+
+#python3 -m pip install PyQt5==5.14
+# python3 -m pip install pyqtdeploy
+#sudo apt-get install python3-setuptools
